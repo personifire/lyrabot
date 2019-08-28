62b55f6a502941a8bfb7b40366623f6b9973d40c
Eric Wang <Eric.Wang.J13@Gmail.com>
2019-08-28 05:45:34 -0500

    Refactored Lyra for discord.py 1.2

--- Lyra.py
+++ Lyra.py
@@ -7,7 +7,7 @@ import sys
 #https://discordapp.com/oauth2/authorize?&client_id=YOUR_CLIENT_ID_HERE&scope=bot&permissions=0
 #https://discordapp.com/oauth2/authorize?&client_id=495322560188252170&scope=bot&permissions=53709888
 
-Client = discord.Client()
+Client = discord.Client(status = "her lyre")
 client = commands.Bot(command_prefix = '!')
 client.remove_command('help')
 
@@ -24,7 +24,6 @@ loop = asyncio.get_event_loop()
 @client.event
 async def on_ready():
     print("Lyra is online")
-    await client.change_presence(game=discord.Game(name='her lyre'))
 
 @client.event
 async def on_resumed():
@@ -32,82 +31,88 @@ async def on_resumed():
 
 @client.event
 async def on_command_error(err, ctx):
-    if isinstance(err, commands.CommandOnCooldown):
-        await client.send_message(ctx.message.channel, "You're on cooldown, " + ctx.message.author.display_name)
+    try:
+        if isinstance(err, commands.CommandOnCooldown):
+            await ctx.message.channel.send("You're on cooldown, " + ctx.message.author.display_name)
+        else:
+            await ctx.message.channel.send("Something bad happened! I can't do that, sorry.")
+            print(err)
+    except Exception as e:
+        print("Exception in on_command_error: \n" + str(e))
 
 @client.event
 async def on_member_remove(member):
-    for channel in member.server.channels:
+    for channel in member.guild.channels:
         if channel.name == "lyra":
             lyra = channel
-    await client.send_message(lyra, member.display_name + ' has left the server')
+    await lyra.send(member.display_name + ' has left the server')
 
 @client.event
 async def on_member_join(member):
-    await client.add_roles(member, discord.utils.get(member.server.roles, name = 'everypony'))
+    await client.add_roles(member, discord.utils.get(member.guild.roles, name = 'everypony'))
 
 @client.event
 async def on_message(message):
     if message.content and not message.author.bot:
         if message.content == "Can I boop you?":
-            await client.send_message(message.channel, "*gasp* but that's lewd!")
+            await message.channel.send("*gasp* but that's lewd!")
 
         elif "screwbot " in message.content.lower() or " screwbot" in message.content.lower() or "screwbot" == message.content.lower():
-            await client.send_message(message.channel, "<a:headcannonL:532554000818634783><:screwbot:532520479290818560><a:headcannonR:532554001321951262>")
+            await message.channel.send("<a:headcannonL:532554000818634783><:screwbot:532520479290818560><a:headcannonR:532554001321951262>")
 
         elif 'hands ' in message.content.lower() or ' hands' in message.content.lower() or 'hands' == message.content.lower():
-            await client.send_message(message.channel, "<:mfw:532548719510552586>")
+            await message.channel.send("<:mfw:532548719510552586>")
 
         elif ':' in message.content.lower():
             new = message.content.lower().replace(":", "")
             if "apples" in new:
-                await client.send_message(message.channel, "<:apples:525139683395764244>")
+                await message.channel.send("<:apples:525139683395764244>")
             elif "angery" in new:
-                await client.send_message(message.channel, "<a:angery:525142734684815370>")
+                await message.channel.send("<a:angery:525142734684815370>")
             elif "blep" in new:
-                await client.send_message(message.channel, "<:blep:532497085254205450>")
+                await message.channel.send("<:blep:532497085254205450>")
             elif "dab" in new:
-                await client.send_message(message.channel, "<:dab:531755608467046401>")
+                await message.channel.send("<:dab:531755608467046401>")
             elif "default" in new:
-                await client.send_message(message.channel, "<a:default:531762705128751105>")
+                await message.channel.send("<a:default:531762705128751105>")
             elif "excite" in new:
-                await client.send_message(message.channel, "<a:excite:525138734145077259>")
+                await message.channel.send("<a:excite:525138734145077259>")
             elif "flyra" in new:
-                await client.send_message(message.channel, "<a:flyra:531755302996148237>")
+                await message.channel.send("<a:flyra:531755302996148237>")
             elif "lyravator" in new:
-                await client.send_message(message.channel, "<a:lyravator:531772198910689280>")
+                await message.channel.send("<a:lyravator:531772198910689280>")
             elif "scared" in new:
-                await client.send_message(message.channel, "<:scared:532497591426875394>")
+                await message.channel.send("<:scared:532497591426875394>")
             elif "swiggityswooty" in new:
-                await client.send_message(message.channel, "<a:swiggityswooty:531779000251580416>")
+                await message.channel.send("<a:swiggityswooty:531779000251580416>")
             elif "thonk" in new:
-                await client.send_message(message.channel, "<:thonk:532534756093460481>")
+                await message.channel.send("<:thonk:532534756093460481>")
 
         elif 'lyra' in message.content.lower() and 'play' in message.content.lower():
             if 'despacito' in message.content.lower():
-                await client.send_message(message.channel, 'https://youtu.be/kJQP7kiw5Fk')
+                await message.channel.send('https://youtu.be/kJQP7kiw5Fk')
             elif 'sinking' in message.content.lower() and 'ships' in message.content.lower():
-                await client.send_message(message.channel, ' https://youtu.be/Tw09Lrf4EQc')
+                await message.channel.send(' https://youtu.be/Tw09Lrf4EQc')
             elif 'lullaby for a princess' in message.content.lower():
-                await client.send_message(message.channel, 'https://youtu.be/H4tyvJJzSDk')
+                await message.channel.send('https://youtu.be/H4tyvJJzSDk')
             elif 'hold on' in message.content.lower():
-                await client.send_message(message.channel, 'https://youtu.be/ryi-Iy0VyWs')
+                await message.channel.send('https://youtu.be/ryi-Iy0VyWs')
             elif 'wow glimmer' in message.content.lower():
-                await client.send_message(message.channel, 'https://youtu.be/12_WnaPmPI0')
+                await message.channel.send('https://youtu.be/12_WnaPmPI0')
             elif 'discord' in message.content.lower():
-                await client.send_message(message.channel, 'https://youtu.be/xPfMb50dsOk')
+                await message.channel.send('https://youtu.be/xPfMb50dsOk')
 
         if client.user.display_name in message.content.lower():
             if 'hi' in message.content.lower() or 'hello' in message.content.lower() or 'sup' in message.content.lower():
-                await client.send_message(message.channel, 'Hi ' + message.author.display_name + '!')
+                await message.channel.send('Hi ' + message.author.display_name + '!')
             if 'rolls' in message.content.lower():
-                await client.send_message(message.channel, "<:smuglyra:543975500562038784>")
+                await message.channel.send("<:smuglyra:543975500562038784>")
         if "u lil shid" in message.content.lower():
-            await client.send_message(message.channel, '*pbbbtthhhhhh*')
+            await message.channel.send('*pbbbtthhhhhh*')
         if "hmmnah.mp4" in message.content.lower():
             await client.send_file(message.channel, "files/hmmnah.mp4")
         if "lyra" in message.content.lower() and "best" in message.content.lower() and "not" not in message.content.lower():
-            await client.send_message(message.channel, '*blushes*')
+            await message.channel.send('*blushes*')
 
         await client.process_commands(message)
 
@@ -182,7 +187,7 @@ async def emote(ctx):
 
 @client.command(pass_context = True)
 async def dm(ctx):
-    await client.send_message(ctx.message.author, "bon")
+    await ctx.message.author.send("bon")
 
 @client.command(pass_context = True)
 async def nuke(ctx):
@@ -218,7 +223,7 @@ async def snuggle(ctx):
 async def rest(ctx):
     if ctx.message.author.id == STAR:
         await client.say("<:sleepytwi:483862389347844116>")
-        id = ctx.message.server.id
+        id = ctx.message.guild.id
         await client.close()
         print('Lyra is offline')
     elif ctx.message.author.id != STAR:
@@ -244,5 +249,9 @@ if __name__ == "__main__":
         print("Process aborted")
 print("Process ended")
 
-client.logout()
+try:
+    client.logout().send(None)
+except StopIteration:
+    print("Client stopped")
+
 quit()
