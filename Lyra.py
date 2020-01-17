aae23d93428a1e6524b6e630bdcc39d4f577e731
Eric Wang <Eric.Wang.J13@Gmail.com>
2020-01-17 07:48:53 +0000

    Added roll extension to bot...

--- Lyra.py
+++ Lyra.py
@@ -6,7 +6,7 @@ import sys
 
 STAR = 410660094083334155
 PERS = 347100862125965312
-EXTENSIONS = ['react', 'fun', 'search', 'vchat', 'uno']
+EXTENSIONS = ['react', 'fun', 'search', 'vchat', 'uno', 'roll']
 
 owners = [PERS]
 
